b51570eb4df34584d2946dc390f9c75cf372a28c
ravi.shankar1975 <ravi.shankar1975@gmail.com>
2013-11-08 13:06:40 +0530

    Added script for monitoring limitslog.
    This script monitors limitlog for "ERROR" and "WARN" messages

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Usage: ./limits_chk.sh (Assign required execute permission using chmod)
 Usage: /limits_chk.sh -d (use -d switch for debugging)
 
 The log file (limitslog) will be available under /var/log/.
+
+Limits Exception Notifier
+=========================
+
+The notifer script, looks for the specified pattern ("WARN" and "ERROR"). If matching pattern is found then an email will
+be send to the given email address.
+
---
+++ limits_chk_notifier.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## This script is used for monitoring limitslog (/var/log/limitslog) for "WARN" and "ERROR" messages.
+## If any log entries found matching the pattern then the details are mailed to the specified email address.
+##
+## Usage: ./limits_chk_notifier.sh (Assign required execute permission using chmod)
+## Author: RShankar
+##
+## ---------------------------------------------------------------------------------------------------------
+## limits_chk_notifer.sh ##
+
+EMAIL_ADDRESS="ravishankar@outlook.in"
+LOG_FILE="/var/log/limitslog"
+PATTERN1="WARN"
+PATTERN2="ERROR"
+
+
+tail -Fn0 $LOG_FILE | \
+while read line ; do
+        echo "$line" | awk $PATTERN1 || $PATTERN2
+        if [ $? = 0 ]
+        then
+             echo $line | mailx -s"limits log report" $EMAIL_ADDRESS
+        fi
+done
